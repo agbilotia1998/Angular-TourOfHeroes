60a632454a7f2fd24f9e4c51bfb56c243e892d0b
agbilotia1998 <iit2016508@iiita.ac.in>
2018-12-09 13:13:49 +0530

    Adds basic functionality and routing

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero";
+
+export const Heroes: Hero[] = [
+  { id: 11, name: 'Mr.Nice' },
+  { id: 12, name: 'Mr.Narco' },
+  { id: 13, name: 'Mr.Bombasto' },
+  { id: 14, name: 'Mr.Celeritas' },
+  { id: 15, name: 'Mr.Magneta' }
+];
